a882f1acf3f7f7a4093403b5ef634a006d1a1ad3
lievi <lievi.silva@gmail.com>
2018-09-30 14:54:03 -0300

    User Profile

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='blog')),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('register/', user_views.register, name="register"),
+    path('blog/', include('blog.urls'), name='blog-home'),
+    path('user/', include('users.urls'), name='users-home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
