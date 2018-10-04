8e7795ca6fc0e1b32d7eb0e91cfd6427b2bb6ab6
lievi <lievi.silva@gmail.com>
2018-10-03 23:35:15 -0300

    .

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-if DEBUG:
+if not DEBUG:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
